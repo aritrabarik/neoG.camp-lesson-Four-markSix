e4aea308fa888b6a9731d6fbd21c947144e4669d
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-09-04 19:31:58 +0530

    ex03: commit

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // console.log("Hello, World!");
 
 //ex02
-var userName = prompt("Enter your name: ");
-alert("Hey there " + userName + ", how are you doing?");
+// var userName = prompt("Enter your name: ");
+// alert("Hey there " + userName + ", how are you doing?");
+
+//ex03
+var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click", function onClick() {
+  console.log("clicked");
+});
