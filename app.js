ce4dc3ef83db91f5f2bc892955805802a3b64d9e
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-09-05 00:08:57 +0530

    ex09: commit

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ var btnTranslate = document.querySelector("#btn-translate");
 btnTranslate.addEventListener("click", function onClick() {
   //   console.log("clicked");
   //   console.log("input: " + inputTxt.value);
-  outputTxt.innerText = "translated: " + inputTxt.value;
+  //   outputTxt.innerText = "translated: " + inputTxt.value;
+
+  var inputText = inputTxt.value; // taking input
+
+  //calling server for processing
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => console.log(json.contents.translated));
 });
 
 //ex04:
@@ -19,3 +27,10 @@ var inputTxt = document.querySelector("#input-txt");
 var outputTxt = document.querySelector("#output-txt");
 
 // outputTxt.innerText = "iGN1T0R";
+
+//ex09:
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
