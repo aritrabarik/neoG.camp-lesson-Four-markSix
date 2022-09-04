1038451595c97ade26b49a46f6673cf4cc006c25
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-09-04 20:11:39 +0530

    ex05: commit

--- app.js
+++ app.js
@@ -7,9 +7,15 @@
 //ex03:
 var btnTranslate = document.querySelector("#btn-translate");
 btnTranslate.addEventListener("click", function onClick() {
-  console.log("clicked");
-  console.log(inputTxt.value);
+  //   console.log("clicked");
+  //   console.log("input: " + inputTxt.value);
+  outputTxt.innerText = "translated: " + inputTxt.value;
 });
 
 //ex04:
 var inputTxt = document.querySelector("#input-txt");
+
+//ex05:
+var outputTxt = document.querySelector("#output-txt");
+
+// outputTxt.innerText = "iGN1T0R";
